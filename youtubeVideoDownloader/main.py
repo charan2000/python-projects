79918af76d10b7d93ab48bac022d41fe9abb9745
charan2000 <charanbhavisetti2000@gmail.com>
2022-09-01 00:52:22 +0530

    some changes in video downloader

--- youtubeVideoDownloader/main.py
+++ youtubeVideoDownloader/main.py
@@ -1,19 +1,18 @@
 from pytube import YouTube
-from sys import argv
+# from sys import argv
 
 save_path = "/home/charan/Downloads"
 
-link = argv[1]
+# link = argv[1]
 
-# link = "youtube.com/watch?v=LXb3EKWsInQ"
+link = "https://youtu.be/luDxcw7dsUM"
 
 yt = YouTube(link)
 
 print(yt.title)
 print(yt.views)
 
+down = yt.streams.get_by_itag(303)
 
-download = yt.streams.get_by_itag(303)
-
-print(round(((download.filesize)/1024)/1024,2))
-download.download(save_path)
+# print(round((download.filesize / 1024) / 1024, 2))
+down.download(save_path)
