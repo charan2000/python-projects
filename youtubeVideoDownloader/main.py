b085a5c9d093aee76f5f0ad69c27b6bc0cf9afb9
charan2000 <charanbhavisetti2000@gmail.com>
2022-09-01 01:38:30 +0530

    some changes in video downloader

--- youtubeVideoDownloader/main.py
+++ youtubeVideoDownloader/main.py
@@ -5,14 +5,13 @@ save_path = "/home/charan/Downloads"
 
 # link = argv[1]
 
-link = "https://youtu.be/luDxcw7dsUM"
-
+link = "https://www.youtube.com/watch?v=luDxcw7dsUM"
 yt = YouTube(link)
 
 print(yt.title)
 print(yt.views)
 
-down = yt.streams.get_by_itag(303)
-
-# print(round((download.filesize / 1024) / 1024, 2))
-down.download(save_path)
+down = yt.streams.get_highest_resolution()
+print(down)
+# print(round((down.filesize / 1024) / 1024, 2))
+down.download(save_path,"NewVid")
