6c5bfac2552fa9d8227d93f300cee8a420c3770b
charlos <charanbhavisetti2000@gmail.com>
2022-10-12 00:32:22 +0530

    Now Suitable for cmd promt exec

--- youtubeVideoDownloader/main.py
+++ youtubeVideoDownloader/main.py
@@ -1,26 +1,25 @@
+import sys
+from sys import argv
 from pytube import YouTube
 
 # from sys import argv
 
 save_path = "/home/charlos/Downloads/"
 
-# link = argv[1]
-
-link = "https://www.youtube.com/watch?v=IPdPUCbnEzs"
+link = argv[1]
+# print(link)
+# link = "https://www.youtube.com/watch?v=LXb3EKWsInQ&t=45s"
 yt = YouTube(link)
 
-print(yt.title)
-print(yt.views)
-
-print(yt.streams.get_highest_resolution())
-print(yt.streams.all)
+# print(yt.streams.get_highest_resolution())
+# print(yt.streams.all)
 # down = yt.streams.get_by_itag("")
-found = True
+
 full_hd_60 = yt.streams.filter(resolution="1080p", mime_type="video/mp4", fps=60)[0]
-full_hd = codex = yt.streams.filter(resolution="1080p", mime_type="video/mp4")[0]
+full_hd = yt.streams.filter(resolution="1080p", mime_type="video/mp4")[0]
 hd_60 = yt.streams.filter(resolution="720p", mime_type="video/mp4", fps=60)[0]
 highest_res = yt.streams.get_highest_resolution()
-#
+
 if full_hd_60:
     codex = full_hd_60
 elif full_hd:
@@ -30,5 +29,14 @@ elif hd_60:
 else:
     codex = highest_res
 
+codex = yt.streams.filter(resolution="1080p", mime_type="video/mp4", fps=60)[0]
+codex_audio = yt.streams.filter(abr="128kbps", mime_type="audio/mp4")[0]
+save_path = save_path + yt.title[:10]
+print("THe Downloaded Files are saved in this path: " + save_path)
 print(codex)
-codex.download(save_path, filename=yt.title[:15])
+print(codex_audio)
+
+codex.download(save_path, filename=yt.title[:10])
+codex_audio.download(save_path, filename=yt.title[:10] + "Audio file")
+
+sys.exit()
